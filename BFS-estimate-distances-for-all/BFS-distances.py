47bffeff599c82f6191042ea3be2b746ef98bee9
psy_ <falkogawantka@gmail.com>
2020-09-01 11:13:47 +0200

    pushed files

---
+++ BFS-estimate-distances-for-all/BFS-distances.py
@@ -0,0 +1,38 @@
+
+
+import sys
+
+
+col0 = [-1,  3,  2, -1]
+col1 = [-1, -1,  5,  2]
+col2 = [-1, -1, -1,  3]
+col3 = [-1, -1, -1, -1]
+
+
+graph = [col0, col1, col2, col3]
+
+
+def getpathsbfs(graph, start, end):
+    queue = []
+    queue.append([start])
+    visited = [-1] * len(graph)
+    path = []
+    d = [sys.maxsize] * len(graph)
+    queue = [start]
+    d[0] = 0
+    while queue:
+        currentnode = queue.pop(0)
+        path.append(currentnode)
+        for neighbornode in range(len(graph)):
+            if graph[currentnode][neighbornode] > 0:
+                if visited[neighbornode] == -1:
+                    visited[neighbornode] = 0
+                    new_path = list(path)
+                    new_path.append(neighbornode)
+                    d[neighbornode] = d[currentnode]+1
+                    queue.append(neighbornode)
+            visited[currentnode] = 1
+    return d
+
+
+print(getpathsbfs(graph, 0, 3))
